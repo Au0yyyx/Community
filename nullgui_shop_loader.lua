af05f7a879432b7eab3f4f3cbdb988250ec62801
Au0yyyx <krypttvxoffic@gmail.com>
2026-08-31 18:53:48 +0100

    Add NULLGUI Shop AI loader

---
+++ nullgui_shop_loader.lua
@@ -0,0 +1,10 @@
+local base = "https://raw.githubusercontent.com/Au0yyyx/Community/main/nullgui_shop/part%d.lua.txt?t=" .. os.time()
+local chunks = table.create(6)
+for index = 1, 6 do
+    chunks[index] = game:HttpGet(string.format(base, index))
+end
+local source = table.concat(chunks)
+local compiled, compileError = loadstring(source, "NULLGUI+ShopAI")
+assert(compiled, compileError)
+return compiled()
+
